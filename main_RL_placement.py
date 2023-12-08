18443f9c382df6d0d1ebcd1e1e27f96f3fd952c3
Jakob <jakob.rat@me.com>
2023-12-08 10:59:11 +0100

    Added stats.

--- main_RL_placement.py
+++ main_RL_placement.py
@@ -35,11 +35,12 @@ from logging.handlers import RotatingFileHandler
 #global variables to control the placement 
 USE_LOGGER = False                  #If True, debug information will be logged under "Logs/{CIRCUIT_NAME}_placement.log".
 INSTANTIATE_CELLS_IN_MAGIC = True   #If True, the devices cell-view will be instantiated in Magic
-N_ITERATIONS = 2500                 #Number of RL-training iterations
+N_ITERATIONS = 1                 #Number of RL-training iterations
 CIRCUIT_FILE = "Circuits/Examples/DiffAmp.spice"    #Input spice-netlist
 CIRCUIT_NAME = "DiffAmp"            #Name of the circuit
 NET_RULES_FILE = "NetRules/net_rules.json"               #Net-rules definition file
 DEF_FILE = None                     #Def file of the circuit
+SHOW_STATS = True                   #Show statistics of the placement
 
 def main():
 
@@ -67,7 +68,7 @@ def main():
     die = MagicDie(circuit=C, def_file=DEF_FILE)
 
     #do the placement by training a RL-agent
-    do_bottom_up_placement(C, N_ITERATIONS, use_weights=False)
+    do_bottom_up_placement(C, N_ITERATIONS, use_weights=False, show_stats=SHOW_STATS)
 
     #save the placed circuit
     file = open(f"PlacementCircuits/{CIRCUIT_NAME}_placement.pkl", 'wb')
