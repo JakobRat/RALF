09a9250bbad845ce067e853b21ad9a24f069baab
Jakob <jakob.rat@me.com>
2023-12-13 16:18:38 +0100

    Tested.

--- Magic/Devices/init_devs.tcl
+++ Magic/Devices/init_devs.tcl
@@ -1,21 +1,16 @@
-load XM1_x3_x1_x1 -silent -quiet
+load XM5 -silent -quiet
 box 0 0 0 0
-::sky130::sky130_fd_pr__pfet_01v8_draw {w 8.0 l 1 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
-select cell XM1_x3_x1_x1
-save XM1_x3_x1_x1
-load XM2_x3_x1_x1 -silent -quiet
+::sky130::sky130_fd_pr__nfet_01v8_draw {w 1.0 l 2 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
+select cell XM5
+save XM5
+load XDP_XM1_XM2 -silent -quiet
 box 0 0 0 0
-::sky130::sky130_fd_pr__nfet_01v8_draw {w 10.0 l 2 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
-select cell XM2_x3_x1_x1
-save XM2_x3_x1_x1
-load XM3_x3_x1_x1 -silent -quiet
+::sky130::sky130_fd_pr__nfet_01v8_draw {w 1.0 l 1 m 1 nf 2 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
+select cell XDP_XM1_XM2
+save XDP_XM1_XM2
+load XDL_XM3_XM4 -silent -quiet
 box 0 0 0 0
-::sky130::sky130_fd_pr__pfet_01v8_draw {w 8.0 l 1 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
-select cell XM3_x3_x1_x1
-save XM3_x3_x1_x1
-load XM4_x3_x1_x1 -silent -quiet
-box 0 0 0 0
-::sky130::sky130_fd_pr__nfet_01v8_draw {w 10.0 l 2 m 1 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 0 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
-select cell XM4_x3_x1_x1
-save XM4_x3_x1_x1
+::sky130::sky130_fd_pr__pfet_01v8_draw {w 2.0 l 1 m 2 nf 1 diffcov 100 polycov 100 guard 1 glc 0 grc 0 gtc 0 gbc 1 tbcov 100 rlcov 100 topc 1 botc 1 poverlap 0 doverlap 1 lmin 0.15 wmin 0.42 full_metal 1 viasrc 0 viadrn 0 viagate 0 viagb 0 viagr 0 viagl 0 viagt 0}
+select cell XDL_XM3_XM4
+save XDL_XM3_XM4
 quit -noprompt
--- Magic/Routing/DiffAmp_RLP_routing.tcl
+++ Magic/Routing/DiffAmp_RLP_routing.tcl
@@ -29,9 +29,9 @@ wire segment m1 30.0 1116 770 1116 770
 wire segment mcon 18.0 1116 770 1116 770
 wire segment m1 30.0 1088 974 1088 974
 wire segment mcon 18.0 1088 974 1088 974
-wire segment m1 28 1116 756 1116 988 -noendcap
-wire segment m1 28 1074 974 1130 974 -noendcap
-box 1102.0 756.0 1130.0 988.0
+wire segment m1 28 1074 770 1130 770 -noendcap
+wire segment m1 28 1088 756 1088 988 -noendcap
+box 1074.0 756.0 1130.0 784.0
 label vmid 1 10 0 0 0 center m1
 wire segment m1 30.0 958 1020 958 1020
 wire segment mcon 18.0 958 1020 958 1020
@@ -41,9 +41,11 @@ wire segment m1 30.0 958 1020 958 1020
 wire segment mcon 18.0 958 1020 958 1020
 wire segment m1 30.0 932 1386 932 1386
 wire segment mcon 18.0 932 1386 932 1386
-wire segment m1 14.0 958 1013 958 1393 -noendcap
-wire segment m1 14.0 925 1386 965 1386 -noendcap
-box 951.0 1013.0 965.0 1393.0
+wire segment m1 14.0 871 1020 965 1020 -noendcap
+wire segment m1 14.0 878 1013 878 1299 -noendcap
+wire segment m1 14.0 871 1292 939 1292 -noendcap
+wire segment m1 14.0 932 1285 932 1393 -noendcap
+box 871.0 1013.0 965.0 1027.0
 label Vop 1 10 0 0 0 center m1
 wire segment m1 30.0 1216 1020 1216 1020
 wire segment mcon 18.0 1216 1020 1216 1020
@@ -53,9 +55,13 @@ wire segment m1 30.0 1216 1020 1216 1020
 wire segment mcon 18.0 1216 1020 1216 1020
 wire segment m1 30.0 1248 1386 1248 1386
 wire segment mcon 18.0 1248 1386 1248 1386
-wire segment m1 14.0 1216 1013 1216 1393 -noendcap
-wire segment m1 14.0 1209 1386 1255 1386 -noendcap
-box 1209.0 1013.0 1223.0 1393.0
+wire segment m1 14.0 1209 1020 1255 1020 -noendcap
+wire segment m1 14.0 1248 1013 1248 1221 -noendcap
+wire segment m1 14.0 1241 1214 1309 1214 -noendcap
+wire segment m1 14.0 1302 1207 1302 1365 -noendcap
+wire segment m1 14.0 1241 1358 1309 1358 -noendcap
+wire segment m1 14.0 1248 1351 1248 1393 -noendcap
+box 1209.0 1013.0 1255.0 1027.0
 label Von 1 10 0 0 0 center m1
 wire segment m1 30.0 886 816 886 816
 wire segment mcon 18.0 886 816 886 816
@@ -69,11 +75,11 @@ wire segment m1 30.0 1000 868 1000 868
 wire segment mcon 18.0 1000 868 1000 868
 wire segment m1 30.0 940 920 940 920
 wire segment mcon 18.0 940 920 940 920
+wire segment m1 28 886 802 886 882 -noendcap
+wire segment m1 28 872 868 954 868 -noendcap
 wire segment m1 28 926 868 1014 868 -noendcap
 wire segment m1 28 940 854 940 934 -noendcap
-wire segment m1 28 872 816 954 816 -noendcap
-wire segment m1 28 940 802 940 882 -noendcap
-box 926.0 854.0 1014.0 882.0
+box 872.0 802.0 900.0 882.0
 label Vss 1 10 0 0 0 center m1
 wire segment m1 30.0 836 1256 836 1256
 wire segment mcon 18.0 836 1256 836 1256
@@ -89,13 +95,8 @@ wire segment m1 30.0 748 1322 748 1322
 wire segment mcon 18.0 748 1322 748 1322
 wire segment m1 28 734 1256 850 1256 -noendcap
 wire segment m1 28 748 1242 748 1336 -noendcap
-wire segment m1 28 822 1256 925 1256 -noendcap
-wire segment m2 38.0 911 1256 911 1256
-wire segment via1 26.0 911 1256 911 1256
-wire segment m1 38.0 911 1256 911 1256
-wire segment m2 28 897 1256 1358 1256 -noendcap
-wire segment m2 38.0 1344 1256 1344 1256
-wire segment via1 26.0 1344 1256 1344 1256
-wire segment m1 38.0 1344 1256 1344 1256
+wire segment m1 28 836 1242 836 1447 -noendcap
+wire segment m1 28 822 1433 1358 1433 -noendcap
+wire segment m1 28 1344 1242 1344 1447 -noendcap
 box 734.0 1242.0 850.0 1270.0
 label Vdd 1 10 0 0 0 center m1
