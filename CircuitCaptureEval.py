d0b1ff0440df13a64b2a8beed46a040772541320
Jakob <jakob.rat@me.com>
2023-12-20 16:59:53 +0100

    Added licence.

--- CircuitCaptureEval.py
+++ CircuitCaptureEval.py
@@ -1,13 +1,30 @@
+# ========================================================================
+#
+#   Script to evaluate the circuit capturing process.
+#
+# SPDX-FileCopyrightText: 2023 Jakob Ratschenberger
+# Johannes Kepler University, Institute for Integrated Circuits
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+# ========================================================================
+
 from SchematicCapture.utils import setup_circuit, include_primitives_hierarchical, get_top_down_topology
-from Magic.utils import instantiate_circuit, add_cells
-from Environment.utils import do_bottom_up_placement
 from SchematicCapture.RString import include_RStrings_hierarchical
-from SchematicCapture.Devices import Device
 
 import matplotlib.pyplot as plt
 import networkx as nx
 
-import tikzplotlib
 
 #global variables to control the placement 
 CIRCUIT_FILE = "Circuits/Examples/CCLatch.spice"    #Input spice-netlist
